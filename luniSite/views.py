5e3b6d2743d76e3c233bb60960210d4a4e6d741c
Samir Faci <dev@esamir.com>
2012-07-05 00:24:34 -0700

    Added FreeGeek, UIC ACM, and UIC LUG to Calendar list.

--- luniSite/views.py
+++ luniSite/views.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 
 
 def Calendar(request):
-	html = """<iframe src="https://www.google.com/calendar/embed?title=FLOSS_Events_In_Chicago&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=luni%40luni.org&amp;color=%23182C57&amp;src=hhlp4gcgvdmifq5lcbk7e27om4%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FChicago" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>"""
+	html = """
+		<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=luni%40luni.org&amp;color=%23182C57&amp;src=hrbebgomk50qksakqkiodnut37vccs91%40import.calendar.google.com&amp;color=%23856508&amp;src=hhlp4gcgvdmifq5lcbk7e27om4%40group.calendar.google.com&amp;color=%23125A12&amp;src=kc72g1ctfg8b88df34qqb62d1s%40group.calendar.google.com&amp;color=%23B1440E&amp;src=ca149os3pmnh0dcopr1jn2negg%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=America%2FChicago" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>		
+		<br/><hr>
+		 * Current aggregation includes PS1, UIC LUG, UIC ACM, FreeGeek. If you're organization has a google calendar or a calendar that gcal can 
+		 import then I can prorbably add it to the list.
+		"""
 	html = getHeader() + html + getSocialHTML()
 	return HttpResponse(html)
 
